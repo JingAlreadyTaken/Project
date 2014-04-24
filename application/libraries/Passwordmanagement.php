6d96716c4432d026e73ac3187c659ed06ab16fce
JingAlreadyTaken <jwu1921@gmail.com>
2014-04-24 19:27:23 -0400

    Second

--- application/libraries/Passwordmanagement.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
-
-class Passwordmanagement {
-
-    public function some_function($string)
-    {
-    	return $string;
-    }
-}
