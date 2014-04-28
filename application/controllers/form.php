8d783bef8004d07e0e501b87f710fccc864baab0
JingAlreadyTaken <jwu1921@gmail.com>
2014-04-27 20:46:50 -0400

    small change

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -4,42 +4,38 @@ class Form extends CI_Controller {
 
 	function index()
 	{
-		//$this->load->helper(array('form', 'url'));
+		$this->load->helper(array('form', 'url'));
 
 		$this->load->library('form_validation');
 
 		$this->load->library('basicpasswordmanagement');
 
-	//	if ($this->form_validation->run() == FALSE)
-	//	{
-			//$this->load->view('form');
-	//	}
-	//	else
-	//	{
-			//$this->load->view('formsuccess');
-	//	}
-			//if($this->form_validation->run() == FALSE)
+
 			$form=$this->input->post();
 			if($form)
 			{
-			/*$entropy=$this->basicpasswordmanagement->entropy($form["password"]);
+
+			$diffValue = $this->basicpasswordmanagement->getPasswordDistance($form["oldpassword"],strlen($form["oldpassword"]),$form["newpassword"],strlen($form["newpassword"]));
+			echo $diffValue;
+				
+			$entropy=$this->basicpasswordmanagement->entropy($form["newpassword"]);
 			echo $entropy.'   :   ';
-			if($this->basicpasswordmanagement->hasorderedcharacters($form["password"],strlen($form["password"])/2)==true)
+			if($this->basicpasswordmanagement->hasorderedcharacters($form["newpassword"],strlen($form["newpassword"])/2)==true)
 				echo "success ordered characters"."<br>";
 			else
 				echo "failure ordered characters"."<br>";
 
-			if($this->basicpasswordmanagement->hasKeyboardOrderedCharacters($form["password"],strlen($form["password"]))==true)
+			if($this->basicpasswordmanagement->hasKeyboardOrderedCharacters($form["newpassword"],strlen($form["newpassword"]))==true)
 				echo "success keyboard ordered characters"."<br>";
 			else
 				echo "failure keyboard ordered characters"."<br>";
 
-			if($this->basicpasswordmanagement->containsString($form["password"],$form["passconf"]) == true)
+			if($this->basicpasswordmanagement->containsString($form["newpassword"],$form["confpassword"]) == true)
 				echo "I failed!!!!";
 			else
 			
 				echo "Yeeee!!!!!";
-			*/
+			
 			echo $this->basicpasswordmanagement->checkPasswordRepetition($form["oldpassword"],$form["newpassword"]);
 			//echo($this->basicpasswordmanagement->strength($form["password"]).':::');
 			}
